9eec61e70449031a3e40c64ebd816fc175fef3ea
ndalecker <ndalecker@precognox.com>
2017-06-03 21:41:27 +0100

    Adding yml2json converter and json2bash_env parser

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM alpine:3.1
-RUN apk --update add curl tar bash wget openssl ca-certificates jq
+RUN apk --update add curl tar bash wget openssl ca-certificates jq py-pip && pip install jsonpipe pyyaml
+#apt-get install python-jsonpipe
 RUN wget --no-check-certificate https://github.com/jwilder/dockerize/releases/download/v0.2.0/dockerize-linux-amd64-v0.2.0.tar.gz
 RUN tar -C /usr/local/bin -xzvf dockerize-linux-amd64-v0.2.0.tar.gz
 
--- bin/etcd-setup.sh
+++ bin/etcd-setup.sh
@@ -23,3 +23,39 @@ for var in $(env | grep KEYS); do
 
     store $etcd_key $etcd_value
 done
+
+function parseDir {
+ for file in $(ls $1); do
+   if [ -f $file ]; then
+     parseFile $file;
+   fi
+ done
+}
+
+function parseFile {
+  if [ ${1: -4} == ".yml" ]; then
+    parseYml $1;
+  fi
+  if [ ${1: -4} == ".json" ]; then
+    parseJson $1;
+  fi
+}
+
+function parseYml {
+   python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < $1 > tmp.json
+   parseJson tmp.json;
+   rm tmp.json;
+}
+
+function parseJson {
+   cat $1 | jsonpipe | grep -v "{}" | grep -v "\[\]" | sed 's/"//g' | sed "s/\s/=/g"
+}
+
+for i in $@; do
+  if [ -f $i ]; then
+    parseFile $i;
+  else 
+    parseDir $i;
+  fi
+done
+
